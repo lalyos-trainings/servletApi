1c9965e596a5ae3dbabc65ee089b780842147f43
lalyos <lalyos@yahoo.com>
2013-11-26 15:57:41 +0100

    ServletContext Listener added for initia;ization

---
+++ src/com/acme/servlet/InitListener.java
@@ -0,0 +1,40 @@
+package com.acme.servlet;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Application Lifecycle Listener implementation class InitListener
+ *
+ */
+public class InitListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public InitListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void contextInitialized(ServletContextEvent ctxEvent) {
+        ServletContext ctx = ctxEvent.getServletContext();
+
+        TreeSet<String> breaktimes = new TreeSet<>();
+        ctx.setAttribute("breakTimes", breaktimes);
+
+        String breaks = ctx.getInitParameter("defaultBreaks");
+        breaktimes.addAll(Arrays.asList(breaks.split(",")));            
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+	
+}
