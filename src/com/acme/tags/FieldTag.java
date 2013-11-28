744b331f0abbf30fc616243788305986a714e806
lalyos <lalyos@yahoo.com>
2013-11-28 10:38:58 +0100

    name attribute added

--- src/com/acme/tags/FieldTag.java
+++ src/com/acme/tags/FieldTag.java
@@ -7,16 +7,31 @@ import javax.servlet.jsp.tagext.TagSupport;
 
 public class FieldTag extends TagSupport {
 
+    private String name;
+    
     @Override
     public int doStartTag() throws JspException {
 
         try {
-            pageContext.getOut().println("<h3>Field tag  ...</h3>");
+            pageContext.getOut().println("<div class=\"control-group\">");
+            pageContext.getOut().println("  <label class=\"control-label\" for=\"input" + name + "\"> " + name + "</label>");
+            pageContext.getOut().println("  <div class=\"controls\">");
+            pageContext.getOut().println("    <input type=\"text\" id=\"input" + name + "\" name=\"" + name + "\" placeholder=\"" + name + "\">");
+            pageContext.getOut().println("  </div>");
+            pageContext.getOut().println("</div>");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         return super.doStartTag();        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     
